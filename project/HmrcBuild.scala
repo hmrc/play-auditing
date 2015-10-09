b935dd230025bf89059eea14be3f65adc30bdf4e
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2015-10-09 16:17:44 +0100

    Implementation code

---
+++ project/HmrcBuild.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt._
+import sbt.Keys._
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc._
+  import DefaultBuildSettings._
+
+  val appName = "play-auditing"
+
+  lazy val microservice = Project(appName, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .settings(
+      targetJvm := "jvm-1.8",
+      libraryDependencies ++= AppDependencies(),
+      crossScalaVersions := Seq("2.11.6"),
+      resolvers := Seq(
+        Resolver.bintrayRepo("hmrc", "releases"),
+        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/"
+      )
+    )
+}
+
+private object AppDependencies {
+
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    "com.typesafe.play" %% "play" % PlayVersion.current,
+    ws,
+    "uk.gov.hmrc" %% "time" % "2.0.0",
+    "uk.gov.hmrc" %% "http-exceptions" % "1.0.0",
+    "uk.gov.hmrc" %% "http-verbs" % "2.2.2-2-gf1fb248"
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test: Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "commons-codec" % "commons-codec" % "1.7" % scope,
+        "org.scalatest" %% "scalatest" % "2.2.4" % scope,
+        "org.scalacheck" %% "scalacheck" % "1.12.2" % scope,
+        "org.pegdown" % "pegdown" % "1.4.2" % scope,
+        "com.github.tomakehurst" % "wiremock" % "1.52" % scope,
+        "uk.gov.hmrc" %% "http-verbs" % "2.2.2-2-gf1fb248"
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
---
+++ src/main/scala/uk/gov/hmrc/play/audit/AuditExtensions.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.play.audit
+
+import uk.gov.hmrc.play.audit.EventKeys._
+import uk.gov.hmrc.play.http.HeaderCarrier
+
+object AuditExtensions {
+  class AuditHeaderCarrier(carrier: HeaderCarrier) {
+    private lazy val auditTags = Map[String, String](
+      carrier.names.xRequestId -> carrier.requestId.map(_.value).getOrElse("-"),
+      carrier.names.xSessionId -> carrier.sessionId.map(_.value).getOrElse("-"),
+      "clientIP" -> carrier.trueClientIp.getOrElse("-"),
+      "clientPort" -> carrier.trueClientPort.getOrElse("-")
+    )
+
+    private lazy val auditDetails = Map[String, String](
+      "ipAddress" -> carrier.forwarded.map(_.value).getOrElse("-"),
+      carrier.names.authorisation -> carrier.authorization.map(_.value).getOrElse("-"),
+      carrier.names.token -> carrier.token.map(_.value).getOrElse("-")
+    )
+
+    def toAuditTags(transactionName: String, path: String) = {
+      auditTags ++ Map[String, String](
+        TransactionName -> transactionName,
+        Path -> path
+      )
+    }
+
+    def toAuditDetails(details: (String, String)*) = auditDetails ++ details
+  }
+
+  implicit def auditHeaderCarrier(carrier: HeaderCarrier) = new AuditHeaderCarrier(carrier)
+}
