77b10714ed434ab4b7c8de5cdc28ad9c73b916f4
Thomas Rynne <thomas.rynne@digital.hmrc.gov.uk>
2018-08-30 10:48:57 +0100

    [TR] TXM-3188 Remove unused library
    async-http-client was only needed for the transitive dependency on slf4j

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -41,7 +41,7 @@ private object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "http-core" % "0.5.0",
-    "com.ning" % "async-http-client" % "1.8.15"
+    "org.slf4j" % "slf4j-api" % "1.7.5"
   )
 
   trait TestDependencies {
