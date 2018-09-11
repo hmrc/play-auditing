6b8822fa48d9e2abbc642a3551c1587a4db45a4f
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2018-09-11 12:28:45 +0100

    PLATOPS-1565 Migration to build.sbt

---
+++ project/AppDependencies.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt._
+
+private object AppDependencies {
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "http-core" % "0.7.0",
+    "org.slf4j" % "slf4j-api" % "1.7.5"
+  )
+
+  val test = Seq(
+    "commons-codec" % "commons-codec" % "1.7" % Test,
+    "org.scalatest" %% "scalatest" % "2.2.6" % Test,
+    "org.scalacheck" %% "scalacheck" % "1.13.1" % Test,
+    "org.pegdown" % "pegdown" % "1.5.0" % Test,
+    "com.github.tomakehurst" % "wiremock" % "1.52" % Test,
+    "org.mockito" % "mockito-all" % "1.10.19" % Test
+  )
+}
